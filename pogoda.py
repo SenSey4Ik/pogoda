e925a44faf845c9de26b9ed8680b20707131da62
SenSey4Ik <massageinekb20@mail.ru>
2025-01-15 13:54:03 +0700

    Асинхрон версия

--- pogoda.py
+++ pogoda.py
@@ -6,21 +6,23 @@ def sinh_pog():
     """Синхронно вызываем get запрос 100 раз"""
 
     #Ссылка на сайт
-    url='https://yandex.ru/pogoda/biysk?lat=52.54181&lon=85.220652'
+    url='https://www.meteovesti.ru/pogoda_10/29947'
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
+    }
 
     for i in range(100):
         #Отправляем запрос на сайт с командой получить
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
 
         # просим у сайта вернуть красиво отформатированный текст
         soup = BeautifulSoup(response.text, 'lxml')
 
         #Парсим класс в котором хранится погода
-        data = soup.find('div', class_= "temp fact__temp fact__temp_size_s")
+        data = soup.find('span', class_="_h3 align-top me-1 d-inline-block").text
 
-        #Вытягиваем температуру
-        temp = data.text
-
-        print(f'В Бийске сейчас: {temp} градусов')
+        print(f'В Бийске: {data} градусов!')
+        print(i)
 
 sinh_pog()
